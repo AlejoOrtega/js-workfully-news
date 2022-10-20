f814bf4a0755b0fc78a00a0d0bd7b4e6646c590a
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-10-20 15:21:29 -0400

    Feature: Article 1 fetchs information

---
+++ js/article1.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    fetch('http://localhost:3000/articles/0')
+    .then(rawData => rawData.json())
+    .then(information => {
+        document.getElementById('title').innerText = information.title;
+        document.getElementById('description').innerText = information.description;
+        document.getElementById('content').innerText = information.content;
+        document.getElementById('author').innerText = information.author;
+        document.getElementById('description').innerText = information.description;
+        document.getElementById('thumbnail').src = information.thumbnail;
+    })
+})
